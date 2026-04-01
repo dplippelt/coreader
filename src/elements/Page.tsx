9b0cd40f4b994f6bdb392f837e0706aab1f19983
dlippelt <dlippelt@student.codam.nl>
2026-04-01 14:19:12 +0200

    Screen states REFACTOR: using new state variable screen which determines in which screen/view state the app is, instead of having separate state variables for each kind of screen/view

--- src/elements/Page.tsx
+++ src/elements/Page.tsx
@@ -1,30 +1,38 @@
-import type { ChapterControls, AppStates } from "../App"
-import type { Chapter as Chap } from "../util/dracula"
+import type { Controls, AppStates } from "../App"
+import type { Chapter as Chap } from "../books/dracula"
+import { Screens } from "../util/utils"
 import StartMenu from "./StartMenu"
 import Chapter from "./Chapter"
 import Navigation from "./Navigation"
 import ChapterSelect from "./ChapterSelect"
+import BookSelect from "./BookSelect"
 
 type PageProps =
 {
 	book: Chap[],
 	states: AppStates,
-	controls: ChapterControls,
+	controls: Controls,
 }
 
 export default function Page( { book, states, controls} : PageProps )
 {
-	if ( states.startMenu )
-		return <StartMenu states={states} controls={controls} />
-
-	if ( states.chapSelect )
-		return <ChapterSelect book={book} states={states} controls={controls}/>
-
-	return (
-		<>
-			<Navigation states={states} controls={controls}/>
-			<Chapter chapter={book[states.currChap]} controls={controls}/>
-			<Navigation states={states} controls={controls}/>
-		</>
-	);
+	switch (states.screen)
+	{
+		case Screens.startMenu:
+			return <StartMenu controls={controls}/>;
+		case Screens.bookSelectMenu:
+			return <BookSelect controls={controls}/>;
+		case Screens.chapSelectMenu:
+			{ return states.bookID ? <ChapterSelect book={book} controls={controls}/> : <BookSelect controls={controls}/> };
+		case Screens.reader:
+			return (
+				<>
+					<Navigation states={states} controls={controls}/>
+					<Chapter chapter={book[states.currChap]} controls={controls}/>
+					<Navigation states={states} controls={controls}/>
+				</>
+			);
+		default:
+			return;
+	}
 }
