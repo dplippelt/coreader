033ac1eb69c345d98fed8a10363f9341a7e2e7f3
dlippelt <dlippelt@student.codam.nl>
2026-04-17 14:50:29 +0200

    WIP - WIP: uploading books

--- src/elements/BookSelect.tsx
+++ src/elements/BookSelect.tsx
@@ -3,6 +3,7 @@ import type { AppStates, Controls } from '../App'
 import { useRef, type ChangeEvent } from 'react'
 import JSZip from 'jszip'
 import { XMLParser } from 'fast-xml-parser'
+import type { Chapter } from '../books/dracula'
 
 type BookSelectProps =
 {
@@ -31,19 +32,75 @@ function cleanFileName( fileName: string ) : string | null
 	return fileName[0].toUpperCase() + fileName.slice(1);;
 }
 
+function getCleanText( html: string ) : string
+{
+	return html
+		.replace(/<[^>]+>/g, " ")
+		.replace(/&nbsp;/g, " ")
+		.replace(/&amp;/g, "&")
+		.replace(/&lt;/g, "<")
+		.replace(/&gt;/g, ">")
+		.replace(/ +/g, " ")
+		.trim();
+}
+
 async function processBook( file: File ) : Promise<void | null>
 {
 	const zip = await JSZip.loadAsync(file);
-	const xmlContainer = await zip.file("META-INF/container.xml")?.async("string");
-	if ( !xmlContainer )
+	const containerXml = await zip.file("META-INF/container.xml")?.async("string");
+	if ( !containerXml )
 		return null;
 
 	const xmlParser = new XMLParser({ ignoreAttributes: false });
-	const parsed = xmlParser.parse(xmlContainer);
-	const opfPath = parsed.container.rootfiles.rootfile["@_full-path"];
+	const containerParsed = xmlParser.parse(containerXml);
+	const opfPath: string = containerParsed.container.rootfiles.rootfile["@_full-path"];
 	if ( !opfPath )
 		return null;
 
+	const opfXml = await zip.file(opfPath)?.async("string");
+	if ( !opfXml )
+		return null;
+
+	const opfParsed = xmlParser.parse(opfXml);
+	const items : [] = opfParsed.package.manifest.item;
+	const navItem = items.find(i => i["@_properties"] === "nav");
+	if ( !navItem )
+		return null;
+
+	const OEBPSdir = opfPath.substring(0, opfPath.lastIndexOf("/") + 1);
+	const navPath = OEBPSdir + navItem["@_href"];
+	const navHtml = await zip.file(navPath)?.async("string");
+
+	console.log(navHtml);
+
+
+
+
+	// const spine: [] = opfParsed.package.spine.itemref;
+	// const idRefs: string[] = [];
+	// for ( let item of spine )
+	// 	idRefs.push(item["@_idref"]);
+
+	// const items : [] = opfParsed.package.manifest.item;
+	// const OEBPSdir = opfPath.substring(0, opfPath.lastIndexOf("/") + 1);
+	// const chapHtmlPaths: string[] = [];
+	// for ( let idRef of idRefs )
+	// {
+	// 	const item = items.find(i => i["@_id"] === idRef);
+	// 	const chapHtmlPath = OEBPSdir + item!["@_href"];
+	// 	chapHtmlPaths.push(chapHtmlPath);
+	// }
+
+	// const chapters: Chapter[] = [];
+	// for ( let i = 0; i < chapHtmlPaths.length; i++ )
+	// {
+	// 	const html = await zip.file(chapHtmlPaths[i])?.async("string");
+	// 	const content = getCleanText(html!);
+	// 	chapters.push({ num: i, title: `Chapter {}`, content: chap_content, questions: questions, music: music });
+
+	// }
+
+	// console.log(chapHtmlPaths);
 
 
 	return;
