23423aa6dd1dc892d6101a620450415b1100f848
dlippelt <dlippelt@student.codam.nl>
2026-04-29 09:39:24 +0200

    Notepad menu & Music player - ADD: notepad menu / button - REFACTOR: extract shared round button style into index.css for both the music player and notepad menu buttons

--- src/elements/MusicButtons.tsx
+++ src/elements/MusicButtons.tsx
@@ -1,5 +1,3 @@
-import styles from './MusicButtons.module.css'
-
 type ButtonProps =
 {
 	onClick: () => void,
@@ -8,8 +6,8 @@ type ButtonProps =
 export function Play( { onClick } : ButtonProps )
 {
 	return (
-		<svg className={styles.musicButton} onClick={onClick} viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
-			<circle className={styles.circle} cx="75" cy="75" r="73"/>
+		<svg className="roundButton" onClick={onClick} viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
+			<circle className="roundButtonCircle" cx="75" cy="75" r="73"/>
 			<g transform="translate(27, 27) scale(4)">
 				<path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z"/>
 			</g>
@@ -20,8 +18,8 @@ export function Play( { onClick } : ButtonProps )
 export function Pause( { onClick } : ButtonProps )
 {
 	return (
-		<svg className={styles.musicButton} onClick={onClick} viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
-			<circle className={styles.circle} cx="75" cy="75" r="73"/>
+		<svg className="roundButton" onClick={onClick} viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
+			<circle className="roundButtonCircle" cx="75" cy="75" r="73"/>
 			<g transform="translate(25, 27) scale(4)">
 				<rect x="14" y="3" width="6" height="18" rx="1"/>
 				<rect x="5" y="3" width="6" height="18" rx="1"/>
@@ -33,8 +31,8 @@ export function Pause( { onClick } : ButtonProps )
 export function SoundOn( { onClick } : ButtonProps )
 {
 	return (
-		<svg className={styles.musicButton} onClick={onClick} viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
-			<circle className={styles.circle} cx="75" cy="75" r="73"/>
+		<svg className="roundButton" onClick={onClick} viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
+			<circle className="roundButtonCircle" cx="75" cy="75" r="73"/>
 			<g transform="translate(26, 27) scale(4)">
 				<path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z"/>
 				<g transform="translate(-1, 0)">
@@ -49,8 +47,8 @@ export function SoundOn( { onClick } : ButtonProps )
 export function SoundOff( { onClick } : ButtonProps )
 {
 	return (
-		<svg className={styles.musicButton} onClick={onClick} viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
-			<circle className={styles.circle} cx="75" cy="75" r="73"/>
+		<svg className="roundButton" onClick={onClick} viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
+			<circle className="roundButtonCircle" cx="75" cy="75" r="73"/>
 			<g transform="translate(26, 27) scale(4)">
 				<path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z"/>
 				<g transform="translate(-1, 0)">
--- src/elements/Reader.tsx
+++ src/elements/Reader.tsx
@@ -3,6 +3,7 @@ import { useSettings } from "./SettingsContext"
 import Book from "./Book"
 import Navigation from "./Navigation"
 import MusicPlayer from "./MusicPlayer"
+import NotepadMenu from "./NotepadMenu"
 import type { AppStates, Controls } from "../App"
 
 type ReaderProps =
@@ -22,6 +23,7 @@ export default function Reader( { book, states, controls } : ReaderProps )
 			<Book book={book} states={states} controls={controls}/>
 			<Navigation states={states} controls={controls}/>
 			{ settings.musicEnabled && states.currChap > 0 ? <MusicPlayer states={states} controls={controls}/> : <></> }
+			<NotepadMenu/>
 		</>
 	);
 }
