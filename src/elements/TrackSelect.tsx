ed932e441e677b6e5ae066353320a7d4c3c613df
dlippelt <dlippelt@student.codam.nl>
2026-05-20 10:27:37 +0200

    Music Player Track List - IMPROVE: Track text in the track list that overflows now scrolls to the left until all text is visible - IMPROVE: Track list is now scrollable vertically if it overflows - FIX: Fix background color of the track text elements to match the background color of the track list element

--- src/elements/TrackSelect.tsx
+++ src/elements/TrackSelect.tsx
@@ -2,6 +2,7 @@ import type { AppStates, Controls } from "../App"
 import styles from "./TrackSelect.module.css"
 import { getBookMusic } from "../books/utils"
 import { MiniPlay } from "./MusicButtons"
+import { useRef, type RefObject } from "react"
 
 type TrackSelectProps =
 {
@@ -18,12 +19,16 @@ type WindowProps =
 {
 	states: AppStates,
 	controls: Controls,
+	textRefs: RefObject<Map<string, HTMLSpanElement>>,
+	wrapperRefs: RefObject<Map<string, HTMLDivElement>>,
 }
 
 type TrackListProps =
 {
 	states: AppStates,
 	controls: Controls,
+	textRefs: RefObject<Map<string, HTMLSpanElement>>,
+	wrapperRefs: RefObject<Map<string, HTMLDivElement>>,
 }
 
 function Menu( { controls } : MenuProps )
@@ -35,9 +40,30 @@ function Menu( { controls } : MenuProps )
 	);
 }
 
-function TrackList( { states, controls } : TrackListProps )
+function TrackList( { states, controls, textRefs, wrapperRefs } : TrackListProps )
 {
 	const trackList = Object.values(getBookMusic(states.currBook));
+
+	function handleMouseEnter( url: string )
+	{
+		const textRef = textRefs.current.get(url);
+		const wrapperRef = wrapperRefs.current.get(url);
+		const scrollWidth = textRef!.scrollWidth;
+		const containerWidth = wrapperRef!.offsetWidth;
+		const scrollDistance = scrollWidth - containerWidth;
+
+		if ( scrollDistance <= 0 )
+		{
+			textRef!.style.setProperty('--scroll-distance', `0px`);
+			textRef!.style.setProperty('--scroll-duration', `0s`);
+			return;
+		}
+
+		const scrollSpeed = 30;
+		const scrollDuration = scrollDistance / scrollSpeed;
+		textRef!.style.setProperty('--scroll-distance', `-${scrollDistance}px`);
+		textRef!.style.setProperty('--scroll-duration', `${scrollDuration}s`);
+	}
 
 	return (
 		<div className={styles.trackList}>
@@ -53,28 +79,33 @@ function TrackList( { states, controls } : TrackListProps )
 							}
 						/>
 					</div>
-					<div>{track.artist} - {track.title}</div>
+					<div ref={ (el) => { if (el) wrapperRefs.current.set(track.url, el) } } className={styles.trackTextWrapper} onMouseEnter={() => handleMouseEnter(track.url)}>
+						<span ref={ (el) => { if (el) textRefs.current.set(track.url, el) } } className={styles.trackText}>{track.artist} - {track.title}</span>
+					</div>
 				</div>
 			))}
 		</div>
 	);
 }
 
-function Window(  { states, controls } : WindowProps )
+function Window(  { states, controls, textRefs, wrapperRefs } : WindowProps )
 {
 	return (
 		<div className={styles.window}>
 			<Menu controls={controls}/>
-			<TrackList states={states} controls={controls}/>
+			<TrackList states={states} controls={controls} textRefs={textRefs} wrapperRefs={wrapperRefs}/>
 		</div>
 	)
 }
 
 export default function TrackSelect( { states, controls } : TrackSelectProps )
 {
+	const textRefs = useRef<Map<string, HTMLSpanElement>>(new Map());
+	const wrapperRefs = useRef<Map<string, HTMLDivElement>>(new Map());
+
 	return (
 		<div className={styles.background}>
-			<Window states={states} controls={controls}/>
+			<Window states={states} controls={controls} textRefs={textRefs} wrapperRefs={wrapperRefs}/>
 		</div>
 	)
 }
